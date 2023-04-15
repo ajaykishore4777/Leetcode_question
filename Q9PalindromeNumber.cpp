7dffc64731be2853576dfcab47052ad673596585
ajaykishore4777 <114057831+ajaykishore4777@users.noreply.github.com>
2023-04-15 23:01:01 +0530

    Update Q9PalindromeNumber.cpp

--- Q9PalindromeNumber.cpp
+++ Q9PalindromeNumber.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
-    }
-};
+        int a=x;
+        if(a<0) return false;
+        long int ans = 0 ;
+        while(a!=0){
+        int digit=a%10;
+        ans = (ans*10)+digit;
+        a=a/10;
+        }
+    
+        if(ans==x) return true;
+        else  return false;   
+}};
+    
