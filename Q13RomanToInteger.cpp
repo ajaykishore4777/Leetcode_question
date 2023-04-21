22ee0bcb6d5315eff54fe35293fcfbf4d1af857c
AJAY KISHORE <114057831+ajaykishore4777@users.noreply.github.com>
2023-04-21 12:00:30 +0530

    commitTt

--- Q13RomanToInteger.cpp
+++ Q13RomanToInteger.cpp
@@ -8,7 +8,7 @@ int romanToInt(string s) {
         {'L',50},                    
         {'C',100},          
         {'D',500},           
-        {'M',1000},
+        {'M',1000},   
     };      
     int ans =0;   
     for(int i=0;i<s.size();i++){
