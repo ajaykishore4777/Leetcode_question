95459dd2cfc0f7c778d6cfc8d5fb7234fdda108d
ajaykishore4777 <114057831+ajaykishore4777@users.noreply.github.com>
2023-05-02 23:20:20 +0530

    23/04/2023

--- Q1TwoSum.cpp
+++ Q1TwoSum.cpp
@@ -5,8 +5,9 @@ public:
             for(int j = i+1;j<nums.size();j++){
             if(nums[i] + nums[j] == target)
              return{i,j};
-          }
+          }  
         }
     return {};
-    }
+    }   
 };
+  
