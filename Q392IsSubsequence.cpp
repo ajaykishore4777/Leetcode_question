dd48bc1a908b612e19aa8fd0d752a3344824a9c8
ajaykishore4777 <114057831+ajaykishore4777@users.noreply.github.com>
2023-05-04 23:00:52 +0530

    03/05/2023

--- Q392IsSubsequence.cpp
+++ Q392IsSubsequence.cpp
@@ -9,9 +9,9 @@ public:
     // of str2 with first unmatched char
     // of str1, if matched
     // then move ahead in str1
-    for (int i = 0; i < t.length() && j < s.length(); i++)
+    for (int i = 0; i < t.length() && j < s.length(); i++)   
         if (s[j] == t[i])
-            j++;
+            j++;  
  
     // If all characters of str1 were found in str2
     return (j == s.length());
