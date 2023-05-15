5ed4cad5d4533354f45e61266bffcabd85e83ae0
ajaykishore4777 <114057831+ajaykishore4777@users.noreply.github.com>
2023-05-15 18:46:41 +0530

    03/05/2023

--- Q392IsSubsequence.cpp
+++ Q392IsSubsequence.cpp
@@ -12,7 +12,7 @@ public:
     for (int i = 0; i < t.length() && j < s.length(); i++)   
         if (s[j] == t[i])
             j++;  
- 
+     
     // If all characters of str1 were found in str2
     return (j == s.length());
     }
