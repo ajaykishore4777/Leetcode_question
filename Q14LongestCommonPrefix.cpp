f12b3316ba0c0ba2a88d104b8b4d7e56e5fe0556
ajaykishore4777 <114057831+ajaykishore4777@users.noreply.github.com>
2023-05-27 23:23:36 +0530

    Update Q14LongestCommonPrefix.cpp

--- Q14LongestCommonPrefix.cpp
+++ Q14LongestCommonPrefix.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         if(strs.size() == 0) return "";
-        int ans = INT_MAX;
+        int ans = INT_MAX;   
         for(int i = 0; i < strs.size()-1; i++){
             ans = min(ans, (int)min(strs[i].size(), strs[i+1].size()));
             while(strs[i].substr(0, ans) != strs[i+1].substr(0, ans)){
