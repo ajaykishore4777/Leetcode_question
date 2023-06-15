c8d6cc9dbe9cf07fa4f39700d8dd24fd39676b7d
AJAY KISHORE <114057831+ajaykishore4777@users.noreply.github.com>
2023-06-15 12:00:30 +0530

    commit

--- 216. Combination Sum III.cpp
+++ 216. Combination Sum III.cpp
@@ -5,23 +5,23 @@ public:
             if(accumulate(cur.begin(), cur.end(), 0) == n){
                 ans.push_back(cur);
             }
-        }else{
+        }else{  
             for(int i = (cur.empty() ? 1 : cur.back()+1); i <= 9; ++i){
                 cur.push_back(i);
-                
+                   
                 backtrack(k, n, cur, ans);
-                
+                  
                 cur.pop_back();
             }
-        }
+        }  
     }
     
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int> cur;
         vector<vector<int>> ans;
         
-        backtrack(k, n, cur, ans);
+        backtrack(k, n, cur, ans);   
         
         return ans;
     }
-};
+};    
