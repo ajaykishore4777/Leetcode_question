4e8861c3c612fe4269a54c2c22ad2af8f08c7de3
AJAY KISHORE <114057831+ajaykishore4777@users.noreply.github.com>
2023-06-27 12:00:30 +0530

    commit

--- 214. Shortest Palindrome.cpp
+++ 214. Shortest Palindrome.cpp
@@ -6,7 +6,7 @@ public:
         for(int i = 0; i < n-1-i; ++i){
             if(s[i] != s[n-1-i]){
                 return false;
-            }
+            }  
         }
            
         return true;
@@ -18,11 +18,11 @@ public:
         for(int l = n; l >= 1; --l){
             if(isPalindrome(s.substr(0, l))){
                 string prepend = s.substr(l);
-                reverse(prepend.begin(), prepend.end());
+                reverse(prepend.begin(), prepend.end());  
                 return prepend + s;
             }    
         }
         
         return "";   
     }
-};
+};   
