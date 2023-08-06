4b3aa345d48b0348cf505b02470ae0b3ee06af85
AJAY KISHORE <114057831+ajaykishore4777@users.noreply.github.com>
2023-08-06 12:00:30 +0530

    commit

---
+++ 219. Contains Duplicate II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        for(int i = 0; i < nums.size(); i++){
+            for(int j=i+1; (j <=i+k) && (j < nums.size()); j++){
+                // cout << "i: " << i << "j: " << j << endl;
+                if(nums[i] == nums[j]) return true;
+            }
+        }
+        // cout << endl;
+        return false;
+    }
+};
