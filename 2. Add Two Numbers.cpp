2fa208e4f1bb7d6f00c8cbfc1bc172a44a3ae170
AJAY KISHORE <114057831+ajaykishore4777@users.noreply.github.com>
2023-08-21 12:00:30 +0530

    commit

---
+++ 2. Add Two Numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *prehead = new ListNode();
+        ListNode *pre = prehead;
+        ListNode *cur1 = l1, *cur2 = l2;
+        int digit = 0;
+        
+        while(cur1 || cur2 || digit > 0){
+            if(cur1){
+                digit += cur1->val;
+                cur1 = cur1->next;
+            }
+            if(cur2){
+                digit += cur2->val;
+                cur2 = cur2->next;
+            }
+            ListNode *tmp = new ListNode(digit%10);
+            pre->next = tmp;
+            pre = pre->next;
+            digit/=10;
+        }
+        
+        return prehead->next;
+    }
+};
